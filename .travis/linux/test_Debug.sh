85556c77e18f26dce264121d122aaaf7ca5efd50
Jan Burle <jan@c53.be>
2017-03-08 10:55:55 +0100

    .build

--- .travis/linux/test_Debug.sh
+++ .travis/linux/test_Debug.sh
@@ -1,6 +1,9 @@
 cd build
 xvfb-run STeCa_tests/STeCa_tests
-lcov -c -d . -o coverage.info
-lcov -r coverage.info 'tests/*' '/opt/*' '/usr/*' -o coverage.info
-lcov -l coverage.info
-coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info
+
+if [ $GXX =0 'g++' ]; then
+  lcov -c -d . -o coverage.info
+  lcov -r coverage.info 'tests/*' '/opt/*' '/usr/*' -o coverage.info
+  lcov -l coverage.info
+  coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info
+fi
