6423c6c3f15b6e031950b1a132421c7dc4b56deb
Jan Burle <jan@c53.be>
2017-03-08 11:02:24 +0100

    .build

--- .travis/linux/build_Debug.sh
+++ .travis/linux/build_Debug.sh
@@ -2,7 +2,12 @@ export QTDIR=/opt/qt54
 export PATH=$QTDIR/bin:$PATH
 
 git submodule update --init --recursive
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE -D WITH_COVERAGE=1 ..
+mkdir build; cd build
+
+if [ $CXX == "g++" ]; then
+  cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE -D WITH_COVERAGE=1 ..
+else
+  cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE ..
+fi
+
 make -j9
