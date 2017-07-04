81c4a346f6776b4c226fd689c18bfd414f3f5754
Jan Burle <jan@c53.be>
2017-07-04 16:36:23 +0200

    disabled loadCaress test

--- .travis/linux/test_Debug.sh
+++ .travis/linux/test_Debug.sh
@@ -1,7 +1,7 @@
 cd build
 xvfb-run tests_steca/tests_steca
 
-if [ $GXX == 'g++' ]; then
+if [ $CXX == 'g++' ]; then
   lcov -c -d . -o coverage.info
   lcov -r coverage.info 'tests/*' '/opt/*' '/usr/*' -o coverage.info
   lcov -l coverage.info
