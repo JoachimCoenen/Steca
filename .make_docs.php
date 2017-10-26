5ed7f6ace0e40aa1798ce58f36a18eaab44911bd
Jan Burle <jan@c53.be>
2017-10-26 11:04:58 +0200

    .

--- .make_docs.php
+++ .make_docs.php
@@ -1,32 +1,81 @@
 #!/usr/bin/php
 <?php
-chdir(dirname(__FILE__).'/src');
+// ensure cwd
+chdir(dirname(__FILE__).'/');
 
-@mkdir($pg = '../docs/pg/');
+// ensure output directory
+@mkdir($pg = './docs/pg/');
 
+// warning
+$FILE = ''; $LINE = 0;
+function error($s) {
+  global $FILE, $LINE;
+  echo "** error in $FILE, line $LINE: $s **\n"; die;
+}
+
+// generate one level of docs
+function docsLevel ($cwd) {
+  global $FILE, $LINE;
+echo ($cwd);
+  if (! ($f = @file($docs = $cwd.'.docs')))
+    error('bad file ' . $docs);
+
+  $lineNo = 0;
+  foreach ($f as $line) {
+    $FILE = $docs; $LINE = ++$lineNo;
+    if (! ($line = trim($line)))
+      continue;
+
+    $ps = array_map('trim', explode("|", $line));
+    if (1 == count($ps)) {  // sublevel
+      list ($dir) = $ps;
+      if (strlen($dir) < 2 || '/' != substr($dir, -1))
+        error('bad dir ' . $dir);
+
+      docsLevel($cwd.$dir);
+    } else {
+      @list ($tag, $outFile, $title, $srcFiles) = $ps;
+      $fs = array_map('trim', explode(",", $srcFiles));
+      if (!$fs)
+        error('no source files');
+    }
+    print_r($ps);
+  }
+  // if (! ($s = @file_get_contents($cwd.'.docs')))
+  //   return;
+  // echo "[$s]";
+}
+
+docsLevel('');
+
+/*
+
+// source data definition
+// [prefix, output file, toc entry, [source files...]]
 $pgSrc = [
-  ['about',     '',               'About Steca', [
-    'manifest.cpp']],
-  ['docs',      'docs.html',      'Documentation', []],
-  ['get',       'get.html',       'Get Steca', []],
-  ['gui',       'gui.html',       'GUI overview', []],
-  ['session',   'session.html',   'Sessions', []],
-  ['workflow',  'workflow.html',  'Workflow', []],
-  ['output',    'output.html',    'Output', []],
-  // [['code', '', 'Code'],],
+  ['about', '', 'About Steca',
+    ['manifest.cpp']],
+  ['docs', 'docs.html', 'Documentation',
+    []],
 ];
 
-$toc = '';
-
+// extract text between /*(begMark) or //(begMark) and (endMark)* / or //(endMark)
 function extractCm ($f) {
-  // between /*:> or //:> and <:*/ or //<:
+  $begMark = '::>'; $endMark = '<::';
+
   $s = file_get_contents($f);
-  preg_match_all("~(/\*:>|//:>)(.*?)(<:\*/|//<:)~s", $s, $res);
+  preg_match_all("~(/\*$begMark|//$begMark)(.*?)($endMark\* /|//$endMark)~s", $s, $res);
+
   return join("\n", $res[2]);
 }
 
+// toc js code
+$toc = '';
+
+// create html pages
 foreach ($pgSrc as $src) {
   list($id, $file, $tx, $fs) = $src;
+
   $toc .= "    ['$id', '$file', '$tx'],\n";
   if (!$file)
     $file = '_index.html';
@@ -63,6 +112,8 @@ $toc  ],
 };
 HEREDOC;
 
+// output toc
 file_put_contents($pg.'toc.js', $toc);
+*/
 
 // eof
