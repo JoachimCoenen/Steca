89dc347df4a4c58cc2c4a0e3dfae68b6da48c420
Jan Burle <jan@c53.be>
2017-11-02 16:12:26 +0100

    .make_docs.php works

--- .make_docs.php
+++ .make_docs.php
@@ -1,18 +1,164 @@
 #!/usr/bin/php
 <?php
+
+const _INDEX_CM     = '_index.cm';
+const _INDEX_HTML   = '_index.html';
+const PRE_TOC       = 'toc.js';
+
+class TocCompiler {
+  private $pages;
+
+  public function __construct ($out) {
+    $this->out = $out;
+  }
+
+  private static function error ($msg) {
+    throw new Exception($msg);
+  }
+
+  public function compile () {
+    try {
+      $i = -1;
+      $this->traverse(null, '', $i);
+      file_put_contents($this->out.PRE_TOC,
+        "book.toc = {lst:[$this->lst], ids:{{$this->ids}}, sec:{{$this->sec}}, pnt:{{$this->pnt}}, fil:{{$this->fil}}};",
+        LOCK_EX);
+    } catch (Exception $e) {
+      error_log(' ** ' . $e->getMessage() . ' **');
+    }
+  }
+
+  // page ids must be unique
+  private $haveIds = [];
+  private function checkUniqueId ($id) {
+    if (@$this->haveIds[$id])
+      self::error('duplicate id: ' . $id);
+    $this->haveIds[$id] = true;
+  }
+
+  private $lst = '', $ids = '', $sec = '', $pnt = '', $fil = '';
+
+  private function getTocLine ($f, $n) { // split by ; into $n parts
+    $l = '';
+    while (!feof($f)) {
+      $l = fgets($f);
+      if ('@toc' != substr($l, 0, 4))
+        continue;
+      $l = substr($l, 4);
+      if (false !== ($pos = strpos($l, '#'))) // cut off comments
+        $l = substr($l, 0, $pos);
+      if (($l = trim($l)))
+        return array_pad(array_map('trim', explode(';', $l)), $n, '');
+    }
+
+    return false;
+  }
+
+  private function traverse ($pntNo, $path, &$i) {
+    $index = $path._INDEX_CM;
+    if (!($f = @fopen($index, 'r')))
+      self::error('bad index: ' . $index);
+
+    @mkdir($this->out.$path);
+    // first @toc entry - index
+    list($id, $title) = $this->getTocLine($f, 2);
+    if (!$id || !$title)
+      self::error('bad toc line in: ' . $index);
+    self::checkUniqueId($id);
+
+    $indNo = ++$i;
+
+    $pf = $path._INDEX_HTML;
+    $this->saveHtml($this->out.$pf, file_get_contents($index));
+
+    $this->lst .= "['$id','$pf','".htmlentities($title)."'],";
+    $this->ids .= "'$id':$i,";
+    $this->sec .= "$i:$indNo,";
+    $this->pnt .= null !== $pntNo ? "$i:$pntNo," : "$i:null,";
+    $this->fil .= "'$pf':$i,";
+
+    // other @toc entries
+    for (;;) {
+      if (false === ($l = $this->getTocLine($f, 3)))
+        break;
+
+      list($idOrSub, $file, $title, $srcFiles) = $l;
+      if ($idOrSub && !$file && !$title && !$srcFiles) {
+        if ('/' === substr($idOrSub, -1)) {
+          $this->traverse($indNo, $path.$idOrSub, $i);
+          continue;
+        }
+      } else if ($idOrSub && $file && $title && $srcFiles) {
+        $id = $idOrSub; ++$i; $file .= '.html';
+        self::checkUniqueId($id);
+        $pf = $path.$file;
+        $this->genHtml($this->out.$pf, $path, $srcFiles);
+        $this->lst .= "['$id','$pf','".htmlentities($title)."'],";
+        $this->ids .= "'$id':$i,";
+        $this->sec .= "$i:$indNo,";
+        $this->pnt .= "$i:$indNo,";
+        $this->fil .= "'$pf':$i,";
+        continue;
+      }
+
+      self::error('bad toc line in: ' . $index);
+    }
+
+    fclose($f);
+  }
+
+  private function genHtml($fname, $path, $srcFiles) {
+    if (! ($srcFiles = array_map('trim', explode(",", $srcFiles))))
+      self::error('bad source file list');
+
+    foreach ($srcFiles as $sf) {
+      if (false === ($src = @file_get_contents($path.$sf)))
+        self::error('bad source file ' . $sf);
+      if ('.cm' != substr($sf, -3))
+        $src = $this->extractCm($src);
+      $this->saveHtml($fname, $src);
+    }
+  }
+
+// extract text between /*(begMark) or //(begMark) and (endMark)* / or //(endMark)
+  function extractCm ($s) {
+    $begMark = '::>'; $endMark = '<::';
+    preg_match_all("~(/\*$begMark|//$begMark)(.*?)($endMark\* /|//$endMark)~s", $s, $res);
+    return join("\n", $res[2]);
+  }
+
+  private function saveHtml ($fname, $tx) {
+    $tx = htmlentities($tx);
+    file_put_contents($fname,
+<<<HEREDOC
+<!DOCTYPE html><html lang="en">
+<head>
+
+<meta charset="utf-8">
+<style>body>pre{display:none;}</style>
+<script>tocjs = '../pg/toc.js'</script>
+<script src ="../CM/load.js"></script>
+
+</head>
+
+<body><pre>
+$tx
+<pre></body>
+</html>
+HEREDOC
+    );
+  }
+
+}
+
+chdir(dirname(__FILE__).'/');               // ensure cwd
+$pages = 'docs/pg/';                        // output directory for pages
+@exec("rm -rf $pages"); mkdir($pages);     // make it empty
+
+(new TocCompiler($pages))->compile();
+
+// eof
 /*
-generates js toc (table-of-content) by scanning the file tree, looking for
-'.docs_toc' files; those are made of lines that are:
-- id | title | source-files ...         (the first line, generates _index.html)
-- id | title | out-file.html | source-files ...   (on subsequent lines)
-- subdir/                               (descend, and search for .docs_toc)
-- # comment
-*/
-
-chdir(dirname(__FILE__).'/');       // ensure cwd
-$pg = 'docs/pg/';                   // output directory for pages
-@exec("rm -rf $pg"); mkdir($pg);    // make it empty
-
 $FILE = ''; $LINE = 0;
 function error ($s) {               // report error and stop
   global $FILE, $LINE;
@@ -132,3 +278,4 @@ HEREDOC
 );
 
 // eof
+*/
