1caec6372b087698526cf9cc6b530f354def7c47
Jan Burle <jan@c53.be>
2017-11-04 13:16:34 +0100

    .make_docs.php works

--- .make_docs.php
+++ .make_docs.php
@@ -1,175 +1,130 @@
 #!/usr/bin/php
 <?php
+/* Traverses the source tree $srcDir and creates the doc tree $docDir:
+- reads _index.cm
+- copies *.cm files over
+- makes *.cm files from specified sources
+*/
 
 $srcDir = dirname(__FILE__).'/';
-$dstDir = $srcDir . 'docs/pg/';
+$docDir = $srcDir . 'docs/pg/';
 
 // make destination directory empty
-@exec('rm -rf ' . $dstDir);
-mkdir($dstDir);
+@exec('rm -rf ' . $docDir);
+mkdir($docDir);
 
-const _INDEX_CM     = '_index.cm';    // source index files
-const _INDEX_HTML   = '_index.html';  // output index files
-const TOC_JS        = 'toc.js';       // comiled table-of-contents
-
-// makes static pages, compiles TOC_JS
-class DocMaker {
-  private $srcDir, $dstDir;
-  public function __construct ($srcDir, $dstDir) {
-    $this->srcDir = $srcDir;
-    $this->dstDir = $dstDir;
-  }
-
-  private static function error ($msg) {
-    throw new Exception($msg);
-  }
-
-  private static function log ($msg) {
-    error_log(' : ' . $msg);
-  }
-
-  public function make () {
-    try {
-      $i = -1; // counts toc entries
-      $this->traverse(null, '', $i, 0);
-      file_put_contents($this->dstDir.TOC_JS,
-        "book.toc = {lst:[$this->lst], ids:{{$this->ids}}, sec:{{$this->sec}}, pnt:{{$this->pnt}}, fil:{{$this->fil}}};",
-        LOCK_EX);
-    } catch (Exception $e) {
-      error_log(' ** ' . $e->getMessage() . ' **');
-    }
-  }
-
-  // page ids must be unique
-  private $haveIds = [];
-
-  private function checkUniqueId ($id) {
-    if (@$this->haveIds[$id])
-      self::error('duplicate id: ' . $id);
-    $this->haveIds[$id] = true;
-  }
-
-  private $lst = '', $ids = '', $sec = '', $pnt = '', $fil = '';
-
-  private function getTocLine ($f, $n) { // split by ';' into $n parts
-    $l = '';
-    while (!feof($f)) {
-      $l = fgets($f);
-      if ('@toc' != substr($l, 0, 4))         // read only toc lines
-        continue;
-      $l = substr($l, 4);
-      if (false !== ($pos = strpos($l, '#'))) // cut off comments
-        $l = substr($l, 0, $pos);
-      if (($l = trim($l)))
-        return array_pad(array_map('trim', explode(';', $l)), $n, '');
-    }
-
-    return false;
-  }
-
-  private function traverse ($pntNo, $path, &$i, $level) {
-    self::log("[$path]");
-    $index = $path._INDEX_CM;
-    if (!($f = @fopen($index, 'r')))
-      self::error('bad index: ' . $index);
-
-    @mkdir($this->dstDir.$path);
-
-    // first @toc entry = index
-    list($id, $title) = $this->getTocLine($f, 2);
-    if (!$id || !$title)
-      self::error('bad toc line in: ' . $index);
-    self::checkUniqueId($id);
-
-    $indNo = ++$i;
-
-    $pf = $path._INDEX_HTML;
-    $this->saveHtml($this->dstDir.$pf, file_get_contents($index), $level);
-
-    $this->lst .= "['$id','$pf','".htmlentities($title)."'],";
-    $this->ids .= "'$id':$i,";
-    $this->sec .= "$i:$indNo,";
-    $this->pnt .= null !== $pntNo ? "$i:$pntNo," : "$i:null,";
-    $this->fil .= "'$pf':$i,";
-
-    // other @toc entries
-    for (;;) {
-      if (false === ($l = $this->getTocLine($f, 4)))
-        break;
-
-      list($idOrSub, $file, $title, $srcFiles) = $l;
-      if ($idOrSub && !$file && !$title && !$srcFiles) {
-        if ('/' === substr($idOrSub, -1)) {
-          $this->traverse($indNo, $path.$idOrSub, $i, $level+1);
-          continue;
-        }
-      } else if ($idOrSub && $file && $title && $srcFiles) {
-        $id = $idOrSub; ++$i; $file .= '.html';
-        self::checkUniqueId($id);
-        $pf = $path.$file;
-        $this->genHtml($this->dstDir.$pf, $path, $srcFiles, $level);
-        $this->lst .= "['$id','$pf','".htmlentities($title)."'],";
-        $this->ids .= "'$id':$i,";
-        $this->sec .= "$i:$indNo,";
-        $this->pnt .= "$i:$indNo,";
-        $this->fil .= "'$pf':$i,";
-        continue;
-      }
-
-      self::error('bad toc line in: ' . $index);
-    }
-
-    fclose($f);
-  }
-
-  private function genHtml($fname, $path, $srcFiles, $level) {
-    if (! ($srcFiles = array_map('trim', explode(",", $srcFiles))))
-      self::error('bad source file list');
-
-    foreach ($srcFiles as $sf) {
-      if (false === ($src = @file_get_contents($path.$sf)))
-        self::error('bad source file ' . $sf);
-      if ('.cm' != substr($sf, -3))
-        $src = $this->extractCm($src);
-      $this->saveHtml($fname, $src, $level);
-    }
-  }
-
-// extract text between /*(begMark) or //(begMark) and (endMark)* / or //(endMark)
-  function extractCm ($s) {
-    $begMark = '::>'; $endMark = '<::';
-    preg_match_all("~(/\*$begMark|//$begMark)(.*?)($endMark\* /|//$endMark)~s", $s, $res);
-    return join("\n", $res[2]);
-  }
-
-  private function saveHtml ($fname, $tx, $level) {
-    self::log(' => ' . $fname);
-    $tx = htmlentities($tx);
-    $toRoot = str_repeat('../', $level);
-    file_put_contents($fname,
-<<<HEREDOC
-<!DOCTYPE html><html lang="en">
-<head>
-
-<meta charset="utf-8">
-<style>body>pre{display:none;}</style>
-<script src ="${toRoot}../CM/load.js"></script>
-
-</head>
-
-<body><pre>
-$tx
-<pre></body>
-</html>
-HEREDOC
-    );
-  }
-
+// helpers
+function msg ($msg) {
+  error_log($msg);
 }
 
-chdir(dirname(__FILE__).'/');               // ensure cwd
-$pages = 'docs/pg/';                        // output directory for pages
+function error ($msg) {
+  msg($msg); die;
+}
 
-(new DocMaker($srcDir, $dstDir))->make();
+function getTocLine ($f, $n) { // split by ';' into $n parts
+  $l = '';
+  while (!feof($f)) {
+    $l = fgets($f);
+    if ('@toc' != substr($l, 0, 4))         // read only toc lines
+      continue;
+    $l = substr($l, 4);
+    if (false !== ($pos = strpos($l, '#'))) // cut off comments
+      $l = substr($l, 0, $pos);
+    if (($l = trim($l)))                    // pad if needed
+      return array_pad(array_map('trim', explode(';', $l)), $n, '');
+  }
+
+  return false;
+}
+
+// extract text between /*(begMark) and (endMark)*/
+// or BETWEEN //(begMark) and //(endMark)
+function findMark($s, $marks) {
+  $pos = false;
+  foreach ($marks as $i => $m) {
+    if (false !== ($p = strpos($s, $m)))
+      if (false === $pos || $p < $pos) {
+        $pos = $p; $mi = $i;  // find the first match in $s
+      }
+  }
+  return false === $pos ? false : [$pos, strlen($marks[$mi])];
+}
+
+function extractCm (&$s) {
+  $begMarks = ['/*::>*/', '/*::>', '//::>'];  // from longer to shorter
+  $endMarks = ['/*<::*/', '<::*/', '//<::'];
+
+  $res = '';
+  while (false !== ($found = findMark($s, $begMarks))) {
+    list ($pos, $len) = $found;
+    $s = substr($s, $pos + $len);
+    if (false === ($found = findMark($s, $endMarks)))
+      error('no end mark');
+    list ($pos, $len) = $found;
+    $res .= substr($s, 0, $pos); $s = substr($s, $pos + $len);
+  }
+
+  return $res;
+}
+
+function genFile ($relPath, $file, $srcFiles) {
+  global $srcDir, $docDir;
+  $relFile = $relPath.$file;
+  msg('> '.$relFile);
+  $s = @file_get_contents($srcDir.$relFile); // can non-exist
+
+  if ($srcFiles)
+    foreach (array_map('trim', explode(",", $srcFiles)) as $sf)
+      if (false === ($src = @file_get_contents($srcDir.$relPath.$sf)))
+        error('bad source file ' . $relPath.$sf);
+      else
+        $s .= extractCm($src);
+
+  if (!$s)  // no read nor generated content
+    error('cannot make ' . $relFile);
+  file_put_contents($docDir.$relFile, $s);
+}
+
+// traverse source tree
+function traverse ($relPath) {
+  global $srcDir, $docDir; $index = '_index.cm';
+  msg(': ' . $relPath);
+  @mkdir($docDir.$relPath);
+
+  if (!($f = @fopen($relPath.$index, 'r')))
+    error('bad index ' . $relPath);
+
+  // first @toc entry -> index
+  list ($id, $title, $srcFiles) = getTocLine($f, 3);
+  if (!$id || !$title)
+    error('bad toc line in: ' . $index);
+
+  genFile($relPath, $index, $srcFiles);
+
+  // other @toc entries
+  for (;;) {
+    if (false === ($l = getTocLine($f, 4)))
+      break;
+
+    list ($idOrSub, $file, $title, $srcFiles) = $l;
+    if ($idOrSub && !$file) {
+      if ('/' === substr($idOrSub, -1)) {
+        traverse($relPath.$idOrSub);
+        continue;
+      }
+    } else if ($idOrSub && $file) {
+      genFile($relPath, $file, $srcFiles);
+      continue;
+    }
+
+    error('bad toc line in: ' . $index);
+  }
+
+  fclose($f);
+}
+
+// do it
+traverse('');
 
 // eof
